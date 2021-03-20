a7bba5f18ced05102010eed09c044cf8c3a0bd49
GraceOyiza <graceoyiza13@gmail.com>
2021-03-20 13:57:23 +0100

    Delete all comment

--- src/index.js
+++ src/index.js
@@ -4,19 +4,11 @@ import renderNav from './nav';
 import renderHome from './home';
 import renderMenu from './menu';
 import renderContact from './contact';
-// I sent a message to you
 
-
-
-// console.log(renderHome(), typeof renderHome())
 const main = document.querySelector('#content')
 const pageContainer = document.createElement('div');
 
-// document.querySelector('#content').appendChild(renderNav());
-// document.querySelector('#content').appendChild(renderHome());
-// document.querySelector('#content').insertAdjacentHTML('beforeend', renderHome());
-// document.querySelector('#content').insertAdjacentHTML('beforeend', renderMenu());
-// document.querySelector('#content').appendChild(renderContact());
+
 
 let currentNode;
 const loadPage = () => {
--- src/nav.js
+++ src/nav.js
@@ -32,4 +32,3 @@ export default () => {
     return nav;
   };
 
-//   export default navigation();
